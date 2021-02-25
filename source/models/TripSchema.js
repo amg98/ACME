2de09d400326c3c2ae029cd910e24ea85c745508
josperdom1 <josperdom1@gmail.com>
2021-02-25 20:48:06 +0100

    feat: Trip schema

---
+++ source/models/TripSchema.js
@@ -0,0 +1,73 @@
+import mongoose, { SchemaTypes } from "mongoose";
+
+const Schema = mongoose.Schema;
+mongoose.set("useCreateIndex", true);
+
+const TripSchema = new Schema(
+  {
+    ticker: {
+      type: String,
+      required: [true],
+      validate: {
+        validator: function (v) {
+          return /(\d{6})-([A-Z]{4})/.test(v);
+        },
+        message: "The ticker format is not valid",
+      },
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: [true],
+    },
+    description: Schema.Types.String,
+    requirements: [String],
+    startDate: {
+      type: Date,
+      required: [true],
+    },
+    endDate: {
+      type: Date,
+      required: [true],
+    },
+    pictures: [String],
+    cancelReason: Schema.Types.String,
+    cancelled: {
+      type: Boolean,
+      required: [true],
+      default: false,
+    },
+    published: {
+      type: Boolean,
+      required: [true],
+      default: false,
+    },
+    stages: [
+      {
+        title: {
+          type: String,
+          required: [true],
+        },
+        description: Schema.Types.String,
+        price: {
+          type: Number,
+          required: [true],
+          min: [0],
+        },
+      },
+    ],
+  },
+  {
+    timestamps: {
+      createdAt: "timeStamp",
+    },
+  }
+);
+
+TripSchema.virtual("price").get(function () {
+  return this.stages.reduce(function (acc, val) {
+    return acc + val.price;
+  }, 0);
+});
+
+module.exports = mongoose.model("Trip", TripSchema);
