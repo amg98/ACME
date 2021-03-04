864af6125491b46dab0d847661087659f042b078
josperdom1 <josperdom1@gmail.com>
2021-03-04 01:21:04 +0100

    feat: Trip post

--- source/App.js
+++ source/App.js
@@ -5,73 +5,73 @@ const DatabaseConnection = require("./DatabaseConnection");
 const SponsorshipController = require("./routes/SponsorshipController");
 const SystemParamsController = require("./routes/SystemParamsController");
 const ActorController = require("./routes/ActorController");
+const TripController = require("./routes/TripController");
 
 class App {
+  constructor() {
+    this.app = express();
+    this.router = express.Router();
+    this.server = null;
+    this.port = process.env.PORT || 8080;
+    this.db = new DatabaseConnection();
 
-    constructor() {
-        this.app = express();
-        this.router = express.Router();
-        this.server = null;
-        this.port = process.env.PORT || 8080;
-        this.db = new DatabaseConnection();
+    this.app.use(cors()); // Needed for SwaggerUI TryIt
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json());
+    this.app.use(this.router);
 
-        this.app.use(cors());       // Needed for SwaggerUI TryIt
-        this.app.use(express.urlencoded({ extended: false }));
-        this.app.use(express.json());
-        this.app.use(this.router);
+    // Route registration
+    const apiPrefix = swagger.getBasePath();
+    SponsorshipController.register(apiPrefix, this.router);
+    SystemParamsController.register(apiPrefix, this.router);
+    ActorController.register(apiPrefix, this.router);
+    TripController.register(apiPrefix, this.router);
 
-        // Route registration
-        const apiPrefix = swagger.getBasePath();
-        SponsorshipController.register(apiPrefix, this.router);
-        SystemParamsController.register(apiPrefix, this.router);
-        ActorController.register(apiPrefix, this.router);
+    this.app.use(App.errorHandler);
 
-        this.app.use(App.errorHandler);
+    swagger.setupSwagger(this.app, this.port);
+  }
 
-        swagger.setupSwagger(this.app, this.port);
-    }
+  static errorHandler(err, req, res, next) {
+    res.status(500).json({ msg: err });
+  }
 
-    static errorHandler(err, req, res, next) {
-        res.status(500).json({ msg: err });
-    }
+  run() {
+    return new Promise((resolve, reject) => {
+      process.on("SIGINT", () => {
+        console.log("[SERVER] Shut down requested by user");
+        this.stop().then(() => {});
+      });
 
-    run() {
-        return new Promise((resolve, reject) => {
+      this.db
+        .setup()
+        .then(() => {
+          this.server = this.app.listen(this.port, () => {
+            console.log(`[SERVER] Running at port ${this.port}`);
+            resolve();
+          });
+        })
+        .catch(reject);
+    });
+  }
 
-            process.on("SIGINT", () => {
-                console.log("[SERVER] Shut down requested by user");
-                this.stop().then(() => { });
-            });
+  stop() {
+    return new Promise((resolve, reject) => {
+      if (this.server == null) {
+        reject();
+        return;
+      }
 
-            this.db.setup()
-                .then(() => {
-                    this.server = this.app.listen(this.port, () => {
-                        console.log(`[SERVER] Running at port ${this.port}`);
-                        resolve();
-                    });
-                })
-                .catch(reject);
-        });
-    }
-
-    stop() {
-        return new Promise((resolve, reject) => {
-
-            if (this.server == null) {
-                reject();
-                return;
-            }
-
-            this.server.close(err => {
-                if(err) {
-                    reject(err);
-                } else {
-                    console.log("[SERVER] Closed successfully");
-                    this.db.close().then(resolve).catch(reject);
-                }
-            });
-        });
-    }
+      this.server.close((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log("[SERVER] Closed successfully");
+          this.db.close().then(resolve).catch(reject);
+        }
+      });
+    });
+  }
 }
 
 module.exports = App;
