dc98653f37964666c635f405cb97499ab88816c0
1nt0 <webdevelopjajo@gmail.com>
2021-03-23 03:11:59 +0100

    Cube completed except computation

--- source/routes/CubeController.js
+++ source/routes/CubeController.js
@@ -1,84 +1,199 @@
 const MonthCube = require("../models/MonthCubeSchema");
 const YearCube = require("../models/YearCubeSchema");
+const Actor = require("../models/ActorSchema");
+const Application = require("../models/ApplicationSchema");
+const Trip = require("../models/TripSchema");
 
+const { CheckAdmin } = require("../middlewares/Auth");
 /**
- * Get the cube amount for given explorer and months
- * @route GET /cubes/{months}
+ * Compute cubes for every explorer and every valid period (M01-M36, Y01-Y03)
+ * @route POST /cubes
  * @group Cubes - Cubes computation
- * @param {string} explorerID.path.required       - Explorer identifier
- * @param {string} month.path.required            - Months to query (M01-M36)
- * @returns {float}                           200 - Returns the amount for the requested cube
- * @returns {DatabaseError}                   500 - Database error
- */
-const getMonthCube = (req, res) => {
-    // Necesita ser un administratorID autenticado
-    // TODO
-};
-
-/**
- * Get the cube amount for given explorer and years
- * @route GET /cubes/{years}
- * @group Cubes - Cubes computation
- * @param {string} explorerID.path.required       - Explorer identifier
- * @param {string} year.path.required             - Years to query (Y01-Y03)
- * @returns {float}                           200 - Returns the amount for the requested cube
- * @returns {DatabaseError}                   500 - Database error
- */
-const getYearCube = (req, res) => {
-  // Necesita ser un administratorID autenticado
-  // TODO
-};
-
-/**
- * Get the explorers that satisfy with given period, money and condition
- * @route GET /cubes/explorers
- * @group Cubes - Cubes computation
- * @param {string} period.path.required           - Valid period (M01-M36 or Y01-Y03)
- * @param {string} amount.path.required           - Amount of money to be evaluated by condition
- * @param {string} condition.path.required        - Condition to be evaluated
- * @returns {Array.<Actor>}                   200 - Returns the explorers that satisfy the condition
- * @returns {DatabaseError}                   500 - Database error
- */
-const getExplorersByConditionCube = (req, res) => {
-  // Necesita ser un administratorID autenticado
-  // TODO
-};
-
-
-/**
- * Compute an n-Month-Cube for a given explorer ID
- * @route POST /cubes/month
- * @group Cubes - Cubes computation
- * @param {MonthCubePost.model} monthCube.body.required   - New month-cube
  * @returns {string}                                  200 - Returns the computed operation
  * @returns {}                                        401 - User is not authorized to perform this operation
  * @returns {DatabaseError}                           500 - Database error
  */
-const computeMonthCube = (req, res) => {
-    // Necesita ser un administratorID autenticado
-    // TODO
+ const computeCubes = async (req, res) => {
+  // Hacerlo con agreggations
+};
+
+
+/**
+ * Get the cube amount for given explorer and months
+ * @route GET /cubes/{actorId}/months/{month}
+ * @group Cubes - Cubes computation
+ * @param {string} actorID.path.required          - Explorer identifier
+ * @param {integer} month.path.required           - Months to query (M01-M36)
+ * @returns {float}                           200 - Returns the amount for the requested cube
+ * @returns {Error}                           400 - Invalid month format
+ * @returns {DatabaseError}                   404 - Cube not found
+ */
+const getMonthCube = (req, res) => {
+
+  const monthRegex = new RegExp("^[mM](0[1-9]|1[0-9]|2[0-9]|3[0-6])$"); //(M01-M36)
+  const isValidMonth = monthRegex.test(req.params.month);
+
+  if (!isValidMonth) {
+    return res.status(400).json({error: "Invalid month format"});
+  }
+
+  const month = req.params.month.substring(1,3);
+
+  Actor.findById(req.params.actorId, function(err, actor) {
+    if (err){
+      return res.status(404).json({"error":"Actor not found", code:err});
+    }
+    else {
+      if(!actor){
+        return res.status(404).json({"error":"Actor not found"});
+      }
+
+      MonthCube.findOne({explorerID: req.params.actorId, month: month}, function(err, monthCube) {
+        if (err){
+          return res.status(404).json({"error":"Cube not found", code:err});
+        }
+        else {
+          if(!monthCube){
+            return res.status(404).json({"error":"Cube was not computed"});
+          }
+
+          res.status(200).json({"Cube amount": monthCube.amount});
+        }
+
+      });
+      
+    }
+  });
 };
 
 /**
- * Compute an n-Year-Cube for a given explorer ID
- * @route POST /cubes/year
+ * Get the cube amount for given explorer and years
+ * @route GET /cubes/{actorId}/years/{year}
  * @group Cubes - Cubes computation
- * @param {YearCubePost.model} yearCube.body.required   - New year-cube
- * @returns {string}                                200 - Returns the computed operation
- * @returns {}                                      401 - User is not authorized to perform this operation
- * @returns {DatabaseError}                         500 - Database error
+ * @param {string} actorID.path.required          - Explorer identifier
+ * @param {integer} year.path.required            - Years to query (Y01-Y03)
+ * @returns {float}                           200 - Returns the amount for the requested cube
+ * @returns {DatabaseError}                   404 - Cube not found
  */
-const computeYearCube = (req, res) => {
-  // Necesita ser un administratorID autenticado
-  // TODO
+const getYearCube = (req, res) => {
+
+  const yearRegex = new RegExp("^[yY](0[1-3])$"); //(Y01-Y03)
+  const isValidYear = yearRegex.test(req.params.year);
+
+  if (!isValidYear) {
+    return res.status(400).json({error: "Invalid year format"});
+  }
+
+  const year = req.params.year.substring(1,3);
+
+  Actor.findById(req.params.actorId, function(err, actor) {
+    if (err){
+      return res.status(404).json({"error":"Actor not found", code:err});
+    }
+    else {
+      if(!actor){
+        return res.status(404).json({"error":"Actor not found"});
+      }
+
+      YearCube.findOne({explorerID: req.params.actorId, year: year}, function(err, yearCube) {
+        if (err){
+          return res.status(404).json({"error":"Cube not found", code:err});
+        }
+        else {
+          if(!yearCube){
+            return res.status(404).json({"error":"Cube was not computed"});
+          }
+
+          res.status(200).json({"Cube amount": yearCube.amount});
+        }
+
+      });
+      
+    }
+  });
 };
 
+/**
+ * Get the explorers that satisfy with given period, money and condition
+ * @route GET /cubes/explorers/{period}/{condition}/{amount}
+ * @group Cubes - Cubes computation
+ * @param {string} period.path.required           - Valid period (M01-M36 or Y01-Y03)
+ * @param {string} amount.path.required           - Amount of money to be evaluated by condition
+ * @param {string} condition.path.required        - Condition to be evaluated (eq, ne, gt, gte, lt, lte)
+ * @returns {Array.<Actor>}                   200 - Returns the explorers that satisfy the condition
+ * @returns {Error}                           400 - Invalid format query
+ * @returns {DatabaseError}                   404 - Explorer not found
+ */
+const getExplorersByConditionCube = async (req, res) => {
+  const period = req.params.period;
+  const condition = req.params.condition;
+  const amount = req.params.amount;
+
+  const possibleConditions = [eq, ne, gt, gte, lt, lte];
+  const periodRegex = new RegExp("^[mM](0[1-9]|1[0-9]|2[0-9]|3[0-6])$|^[yY](0[1-3])$"); //(M01-M36 or Y01-Y03)
+  const isValidPeriod = periodRegex.test(period);
+
+  if (!possibleConditions.includes(condition)) {
+    return res.status(400).json({error: "Invalid condition format"})
+  }
+  if (!isValidPeriod) {
+    return res.status(400).json({error: "Invalid period format"})
+  }
+  if (isNaN(amount)) {
+    return res.status(400).json({error: "Invalid amount format"})
+  }
+
+  var explorers = [];
+  var parsedCondition = {};
+  parsedCondition["$"+condition] = amount;
+
+  if(period.startsWith('M')) {
+    var monthNumber = period.substring(1,3);
+    const cubes = await MonthCube.find({ month:monthNumber, amount: parsedCondition});
+
+    cubes.forEach((cube) => {
+      Actor.findOne({_id: cube.explorerID}, function(err, explorer) {
+        if (err){
+          return res.status(404).json({"error":"Explorer not found", code:err});
+        }
+        else {
+          if(!explorer){
+            return res.status(404).json({"error":"Explorer not found"});
+          }
+          explorers.push(explorer);
+        }
+      });
+    });
+  } else {
+    var yearNumber = period.substring(1,3);
+    const cubes = await YearCube.find({ year: yearNumber, amount: parsedCondition});
+
+    cubes.forEach((cube) => {
+      Actor.findOne({_id: cube.explorerID}, function(err, explorer) {
+        if (err){
+          return res.status(404).json({"error":"Explorer not found", code:err});
+        }
+        else {
+          if(!explorer){
+            return res.status(404).json({"error":"Explorer not found"});
+          }
+          explorers.push(explorer);
+        }
+      });
+    });
+  }
+  
+  return res.status(200).json({"explorers": explorers});
+
+};
 
 
 module.exports.register = (apiPrefix, router) => {
   const apiURL = `${apiPrefix}/cubes`;
-  router.get(apiURL+'/:actorId', getActor);
-  //TODO
+  router.post(apiURL, CheckAdmin, computeCubes);
+  router.get(apiURL+'/:actorId/months/:month', CheckAdmin, getMonthCube);
+  router.get(apiURL+'/:actorId/years/:year', CheckAdmin, getYearCube);
+  router.get(apiURL+'/explorers/:period/:condition/:amount', CheckAdmin, getExplorersByConditionCube);
 };
 
 /**
@@ -96,7 +211,7 @@ module.exports.register = (apiPrefix, router) => {
  * @property {string} _id                       - Unique identifier (ignored in POST requests due to id collision)
  * @property {integer} amount.required          - Amount of money spend on given months
  * @property {integer} month.required           - Number of months (1-36)
- * @property {string} explorerID.required       - Explorer ID
+ * @property {string} actorID.required          - Explorer ID
  */
 
  /**
@@ -104,13 +219,9 @@ module.exports.register = (apiPrefix, router) => {
  * @property {string} _id                       - Unique identifier (ignored in POST requests due to id collision)
  * @property {integer} amount.required          - Amount of money spend on given years
  * @property {integer} year.required            - Number of years (1-3)
- * @property {string} explorerID.required       - Explorer ID
+ * @property {string} actorID.required          - Explorer ID
  */
 
- /**
- * @typedef Rol
- * @property {string} rol.required          - User role (explorer, manager, sponsor, administrator)
- */
 
 /**
  * @typedef Actor
